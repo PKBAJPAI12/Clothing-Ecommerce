728bd35c0aa4ef7ea2e7fb9d75623f6acef6b8c0
PKBAJPAI12 <prateek.cse.uiet@gmail.com>
2022-06-06 01:36:22 +0530

    I have added modal and javascript and make add to cart feature

--- src/main/java/project/Dao/ProductDao.java
+++ src/main/java/project/Dao/ProductDao.java
@@ -3,29 +3,47 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import project.Model.Product;
+import org.hibernate.query.*;
+
+import java.util.List;
+
 public class ProductDao {
     private SessionFactory factory;
 
     public ProductDao(SessionFactory factory) {
         this.factory = factory;
     }
+    public List<Product> getAllProducts(){
+        Session s = this.factory.openSession();
+        Query query = s.createQuery("FROM Product ");
+        List<Product> list=query.list();
+        return list;
 
-   public boolean saveProduct(Product product) {
-        boolean f = false;
-        try {
-            Session session = this.factory.openSession();
-            Transaction tx = session.beginTransaction();
-            session.save(product);
-            tx.commit();
-            session.close();
-            f = true;
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            f = false;
-        }
-        return  f;
     }
-
-
+    public List<Product> getProductsByCatName(String catName){
+        Session s = this.factory.openSession();
+        Query query = s.createQuery("FROM Product where pCategory=:name");
+        query.setParameter("name",catName);
+        List<Product> list=query.list();
+        return list;
+    }
+    public List<Product> getProductsByColName(String colName){
+        Session s = this.factory.openSession();
+        Query query = s.createQuery("FROM Product where pCollection=:name");
+        query.setParameter("name",colName);
+        List<Product> list=query.list();
+        return list;
+    }
+    public List<Product> getProductsByLowPrice(){
+        Session s = this.factory.openSession();
+        Query query = s.createQuery("FROM Product as p order by  p.pPrice ASC");
+        List<Product> list=query.list();
+        return list;
+    }
+    public List<Product> getProductsByHighPrice(){
+        Session s = this.factory.openSession();
+        Query query = s.createQuery("FROM Product as p order by  p.pPrice DESC");
+        List<Product> list=query.list();
+        return list;
+    }
 }
